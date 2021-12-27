571e930dfd181c2f46f70ad60471ca4ecb3a2689
Romane Delaborde <romane.delaborde@gmail.com>
2021-12-27 11:52:27 +0100

    Petite mise à jour

--- enigme.hh
+++ enigme.hh
@@ -3,7 +3,6 @@
 #include <iostream>
 #include "carte.hh"
 
-// carte Enigme est la carte où il faut "rentrer" la solution mais une énigme correspond à plusieurs cartes
 
 class Enigme : public Carte{
 
