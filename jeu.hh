def726e659c3716be646a3a06d6027709736d93b
Romane Delaborde <romane.delaborde@gmail.com>
2021-12-27 18:47:53 +0100

    Ajout de méthodes en commentaires

--- jeu.hh
+++ jeu.hh
@@ -2,18 +2,19 @@
 
 #include <iostream>
 #include <set>
+#include <map>
 
 
 class Jeu{
 
 private:
 
-  std::set<Carte> _cartes_affichees; // ensemble des cartes affichées à l'écran
+  std::map<Carte> _cartes_jeu; // ensemble des cartes du jeu : bool pour dire si elles sont affichées à l'écran ou non
 
 public:
 
   // Constructeur
-  Jeu(Carte carte_regles); // ou alors ne rien mettre dedans comme la carte des règles est connue et tjrs la 1ère
+  Jeu(Carte carte_regles); // ou alors ne rien mettre dedans
 
   // Destructeur
   ~Jeu(){};
@@ -23,4 +24,8 @@ public:
   // retrait carte
   // màj valeurs clés de chaque map des cartes contenues dans _cartes_affichees
   // affichage des cartes ? -> voir tous les bails graphiques à faire
+
+  // charger les dicos de chaque carte à partir du ou des fichiers csv -> peut-être il va falloir faire un switch si on a plusieurs csv
+  // méthode pour taper les codes : s'ils sont valides alors on débloque des cartes
+
 };
