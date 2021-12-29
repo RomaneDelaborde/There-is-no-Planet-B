701d9c34c66a96d2b595556f3d677a5c9f54bcd6
Romane Delaborde <romane.delaborde@gmail.com>
2021-12-29 15:01:13 +0100

    Modifications de noms de variables et ajout de méthodes en lien avec la classe Jeu

--- jeu.cc
+++ jeu.cc
@@ -1,7 +1,86 @@
 #include "jeu.hh"
 
 
-void changement_valeur_carte_dico(int id_carte, bool est_dispo)
+Jeu::Jeu()
 {
-  // faire un for en comparant chaque id de chaque carte avec l'id passé en paramètres -> doit être coûteux ...
+  // remplissage _cartes_jeu et __map_id en même temps (soit ligne par ligne à la main soit en passant par un fichier csv)
+  // toutes les valeurs de _map_id sont initialisées à 0 sauf la carte règles qui vaut 1
+  // vérifier qu'il y ait le même nb d'éléments dans _cartes_jeu et _map_id après la création de ces 2 conteneurs
+
+}
+
+bool id_existe(const int id_carte) const
+{
+  if(_map_id.find(carte(id_carte))==_map_id.end()) // si l'id n'existe pas
+  {
+    //std::cout << "ERREUR : l'id entré en paramètres ne correspond à aucune carte" << std::endl;
+    return 0;
+  }
+  return 1;
+}
+
+
+
+Carte Jeu::carte(const int id_carte) const // à partir d'une valeur d'id d'une carte (unique), renvoit sa carte correspondante
+{
+  for(int i=0; i< _cartes_jeu.size(); i++) // boucle for mais peut-être while c'est mieux ?
+  {
+    if(_cartes_jeu[i]._id==id_carte) // si on a trouvé un match des id, on retourne la carte correspondante
+    {
+      return _cartes_jeu[i];
+    }
+  }
+  // si on a pas trouvé un match des id, on affiche un message d'erreur et on retourne un pointeur vide
+  std::cout << "ERREUR : l'id entré en paramètres ne correspond à aucune carte" << std::endl;
+  return std::nullptr; // return NULL ou dans l'idéal faire un "throw no_such_id_error"
+}
+
+
+void Jeu::carte_map_affichage(const int id_carte) // on pourrait faire 1 seule fonction pour affichage/désaffichage mais humain peut plus facilement se mélanger les pinceaux dans les valeurs
+{
+  if(!id_existe(id_carte)) // si l'id n'existe pas
+  {
+    return;
+  }
+  _map_id[carte(id_carte)]=1;
+}
+
+void Jeu::carte_map_desaffichage(const int id_carte)
+{
+  if(!id_existe(id_carte)) // si l'id n'existe pas
+  {
+    return;
+  }
+  _map_id[carte(id_carte)]=-1;
+}
+
+
+void Jeu::demande_affichage_carte(int id_carte)
+{
+  if(!id_existe(id_carte)) // si l'id n'existe pas
+  {
+    // affichage pop-up pour le joueur qqpart du type "Cet id ne correspond à aucune carte."
+    return;
+  }
+
+  switch(_map_id[carte(id_carte)]){
+    case 1:
+      //std::cout << "ERREUR : la carte est déjà affichée" << std::endl;
+      // affichage pop-up pour le joueur qqpart du type "La carte souhaitée est déjà affichée."
+      break;
+
+    case -1:
+      //std::cout << "ERREUR : la carte a déjà été affichée" << std::endl;
+      // affichage pop-up pour le joueur qqpart du type "La carte souhaitée a été défaussée car elle n'est plus utile."
+
+      break;
+
+    case 0:
+      break;
+
+    //default:
+      //break;
+
+  }
+  return;
 }
--- jeu.hh
+++ jeu.hh
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include "carte.hh"
+#include "objet.hh"
+#include "enigme.hh"
+// #include "inventaire.hh"
 
 
 class Jeu{
@@ -10,45 +14,35 @@ class Jeu{
 private:
 
   std::vector<Carte> _cartes_jeu; // liste des cartes du jeu
-  std::map<int, bool> _id_cartes; //map des id des cartes du jeu : bool pour dire si elles sont affichées à l'écran ou non
-  // ou alors faire un ensemble/liste(*) avec les cartes et le dico _carte_jeu contient des id en clés pour faciliter les choses et après on regarde les attributs dont on a besoin en parcourant chaque élément de (*)
+  std::map<int, int> _map_id; //map des id des cartes du jeu : int pour donner une indication sur leur affichage : 1 = carte affichée, 0 = carte pas encore affichée et -1 = carte affichée dans le passé et pas réaffichable
+  // toutes les valeurs de _map_id sont initialisées à 0 sauf la carte règles qui vaut 1
 
 public:
 
-  // Constructeur
-  Jeu(Carte carte_regles); // ou alors ne rien mettre dedans
+  // Constructeur sans paramètres pour l'instant : quels paramètres à mettre dedans sinon ?
+  Jeu();
 
-  // Destructeur
+  // Destructeur (vide normalement)
   ~Jeu(){};
 
-  // Méthodes (des getter vont peut-être être nécessaires pour accéder à certains attributs privés)
-  Carte carte_id(int id_carte);
-  /*
-  {
-    if(id_carte n'est pas dans les clés de _id_cartes)
-    {
-      alors id_carte n'existe pas, on affiche un message d'erreur et on exit en amont
-    }
-    for(int i=0; i< _cartes_jeu.size(); i++)
-    {
-      if(_cartes_jeu[i]._id==id_carte)
-      {
-        return _cartes_jeu[i];
-      }
-    }
+  // Méthodes (des getter vont peut-être être nécessaires dans d'autres classes pour accéder à certains attributs privés)
 
-  }
-  */
+  bool id_existe(const int id_carte) const; // renvoie 1 si l'id est dans _map_id (si id existe alors carte correspondante existe aussi normalement)
+  Carte carte(const int id_carte) const; // à partir d'une valeur d'id d'une carte (unique), renvoit sa carte correspondante si celle-ci existe
 
-  void changement_valeur_carte_dico(int id_carte, bool dispo); // ou faire 2 fonctions explicites : dispo et pas_dispo (dispo pour disponible)
-  // ajout carte dans les cartes affichées
-  // retrait carte
+  void carte_map_affichage(const int id_carte); // dans _map_id la valeur de la clé correspondant à id_carte passe à la valeur 1
+  void carte_map_desaffichage(const int id_carte); // dans _map_id la valeur de la clé correspondant à id_carte passe à la valeur -1
+
+  void demande_affichage_carte(int id_carte); // demande de l'affichage d'une carte par le joueur
+  //bool affichage_carte_autorise(); // indique si la carte peut-être affichée ou non --> méthode utile ou pas ?
+
+
   // màj valeurs clés de chaque map des cartes contenues dans _cartes_affichees
   // affichage des cartes ? -> voir tous les bails graphiques à faire : ça serait bien affichage carte en fonction de l'id
 
-  //bool affichage_carte_autorise(); // indique si la carte peut-être affichée ou non
+
 
   // charger les dicos de chaque carte à partir du ou des fichiers csv -> peut-être il va falloir faire un switch si on a plusieurs csv
-  // méthode pour taper les codes : s'ils sont valides alors on débloque des cartes
+  // méthodes pour taper les codes : s'ils sont valides alors on débloque des cartes
 
 };
