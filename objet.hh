bb0f595c2ba543544991c3d1b56a72d2da985a42
Romane Delaborde <romane.delaborde@gmail.com>
2021-12-29 22:05:40 +0100

    Ajout commentaire : classe pas à jour

--- objet.hh
+++ objet.hh
@@ -6,6 +6,8 @@
 
 class Objet : public Carte{
 
+// Classe pas à jour
+
 private:
   bool _union; // vaut 1 s'il faut combiner juste l'un des objets combinable de la liste et 0 s'il faut les combiner au moins une fois avec tous les objets de la liste
   std::map<Objet, Carte> _objets_combinables; // map : en clé les objets combinables avec cet objet (map d'Objet ou d'id pour faciliter les choses ??) et en valeur : Carte (ou id_carte ??) de la carte résultante de cette union
