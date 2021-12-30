491f29462e1b0931538cdfac712e4358894b1267
Romane Delaborde <romane.delaborde@gmail.com>
2021-12-30 20:19:56 +0100

    Ajout include fichiers hh

--- main.cc
+++ main.cc
@@ -1,4 +1,11 @@
 # include <iostream>
+#include "jeu.hh"
+#include "carte.hh"
+#include "objet.hh"
+#include "enigme.hh"
+#include "enigme1.hh"
+#include "enigme2.hh"
+#include "enigme3.hh"
 
 int main()
 {
