e3040d552c5e86b5f960bea6f6c13a369e11ece7
Romane Delaborde <romane.delaborde@gmail.com>
2021-12-30 17:55:28 +0100

    int devient std::size_t dans les boucles for

--- carte.cc
+++ carte.cc
@@ -5,13 +5,13 @@ void Carte::affichage_info_carte() const
   std::cout << "Nom carte : " << _nom_carte << std::endl;
   std::cout << "Id : " << _id << std::endl;
   std::cout << "Id cartes suivantes : ";
-  for(int i=0; i<_id_cartes_posterieures.size();i++)
+  for(std::size_t i=0; i<_id_cartes_posterieures.size();i++)
   {
     std::cout << _id_cartes_posterieures[i] << ' ';
   }
   std::cout << std::endl;
   std::cout << "Id cartes kick : ";
-  for(int i=0; i<_id_cartes_kick.size();i++)
+  for(std::size_t i=0; i<_id_cartes_kick.size();i++)
   {
     std::cout << _id_cartes_kick[i] << ' ';
   }
