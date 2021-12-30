fcdd015b93e9799abad4511d6fe19a8bf27b6a74
Romane Delaborde <romane.delaborde@gmail.com>
2021-12-30 16:35:59 +0100

    Ajout méthodes énigme 2 (pas de tests réalisés pour le moment)

--- enigme2.cc
+++ enigme2.cc
@@ -1,1 +1,68 @@
 #include "enigme2.hh"
+
+Enigme1::Enigme1(std::string nom_carte, int id, int id_enigme, int nb_essais, int code_solution, std::string phrase):Enigme(nom_carte, id, id_enigme, nb_essais, code_solution), _phrase(phrase) // mettre Enigme::Enigme ou pas ??
+{
+  std::string alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  for(std::string::size_type i=0; i<alphabet.length(); i++)
+  {
+      _alphabet_indices[alphabet[i]]=i+1; // this-> nécessaire ou pas ??
+  }
+}
+
+
+
+
+void Enigme1::codage_jules_cesar(int decalage, bool sens_decalage)
+{
+  _phrase_codee="";
+
+    for(std::string::size_type i=0; i<_phrase.length(); i++)
+    {
+        if(_phrase[i]==' ')
+        {
+          _phrase_codee+=' ';
+        }
+
+        else
+        {
+          int indice=_alphabet_indices[_phrase[i]];
+
+          if(sens_decalage) // vers la droite si sens_decalage=1
+          {
+            indice+=decalage%_alphabet_indices.size();
+
+          }
+          else
+          {
+            indice-=decalage%_alphabet_indices.size();
+          }
+
+          /*
+          for(std::map<char,int>::iterator iter = _alphabet_indices.begin(); iter != _alphabet_indices.end(); ++iter)
+          {
+            char c =  iter->first; // c pour clé
+            int v = iter->second; // v pour valeur
+            if(v==indice)
+            {
+              _phrase_codee+=v;
+            }
+          }
+          */
+          std::map<char,int>::iterator iter = _alphabet_indices.begin();
+          while(iter != _alphabet_indices.end())
+          {
+            char c =  iter->first; // c pour clé
+            int v = iter->second; // v pour valeur
+            if(v==indice)
+            {
+              _phrase_codee+=v;
+              break;
+            }
+            ++iter;
+          }
+
+
+        }
+    }
+}
--- enigme2.hh
+++ enigme2.hh
@@ -1,15 +1,31 @@
 #pragma once
 
 #include <iostream>
+#include <map>
 #include "enigme.hh"
 
 
 class Enigme2 : public Enigme{ // énigme avec le livre de Roald Dahl
 
 private:
+  std::map<char, int> _alphabet_indices; // map pour ne pas balayer l'alphabet à chaque fois qu'on cherche l'indice d'une lettre
+  std::string _phrase;
+  std::string _phrase_codee;
 
 
 public:
+
+  // Constructeur
+  Enigme1(std::string nom_carte, int id, int id_enigme, int nb_essais, int code_solution, std::string phrase);
+
+  // Destructeur : bail virtuel ?? ou alors hérite direct de celui de la classe mère ??
+
+  // Getter
+  std::string get_phrase() const {return _phrase};
+  std::string get_phrase_codee() const {return _phrase_codee};
+
+  // Méthodes
+  std::string codage_jules_cesar(int decalage, bool sens_decalage);
 
 
 
