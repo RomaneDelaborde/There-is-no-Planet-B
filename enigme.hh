7175cbfbe177cdb66c982dd16740057a685f0cfc
Romane Delaborde <romane.delaborde@gmail.com>
2021-12-30 16:38:01 +0100

    Ajout commentaire pour attribut _nb_essais

--- enigme.hh
+++ enigme.hh
@@ -9,7 +9,7 @@ class Enigme : public Carte{
 private:
   int _id_enigme; // code unique à chaque énigme : 1, 2 ou 3
   int _code_solution; // code de la solution de l'énigme : code ou id d'objet (Charlie) -> à ne pas mettre dans le constructeur car calculé par des méthodes sauf pour Charlie : 2 constructeurs à faire
-  int _nb_essais; // nombre d'essais pour réussir l'énigme
+  int _nb_essais; // nombre d'essais pour réussir l'énigme (peut être différente pour chaque énigme mais le préciser au joueur dans ce cas)
 
 
 public:
@@ -19,13 +19,13 @@ public:
 
   // Destructeur : bail virtuel ?? ou alors hérite direct de celui de la classe mère ??
 
-  // Méthodes
-  // Méthodes virtuelles communes : calcul_code_solution ?
   // Getter
 
   int get_id_enigme() const {return _id_enigme};
   int get_code_solution() const {return _code_solution};
   int get_nb_essais() const {return _nb_essais};
 
+  // Méthodes
+  // Méthodes virtuelles communes : calcul_code_solution ?
 
 };
