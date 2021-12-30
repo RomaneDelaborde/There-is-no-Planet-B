15fbb184be59ae75bb93eb17aef6839df6af184f
Romane Delaborde <romane.delaborde@gmail.com>
2021-12-30 17:02:17 +0100

    Ajout attributs et méthodes pour énigme3

--- enigme3.cc
+++ enigme3.cc
@@ -1,1 +1,64 @@
 #include "enigme3.hh"
+
+Enigme2::Enigme3(std::string nom_carte, int id, int id_enigme, int nb_essais, std::string chaine):Enigme(nom_carte, id, id_enigme, nb_essais, code_solution), _chaine(chaine) // mettre Enigme::Enigme ou pas ??
+{
+  _lettres= {{'V', 2}, {'T', 3}, {'J', 5}, {'S', 6}, {'U', 7}, {'N', 8}, {'B', 9}}; // this-> nécessaire ou pas ??
+}
+
+bool Enigme3::lettre_existe(char l) const // l pour lettre
+{
+  if(_lettres.find(l)==_lettres.end()) // si lettre n'existe pas dans les clés de _lettres
+  {
+    //std::cout << "ERREUR : la lettre entrée en paramètres n'existe pas dans les clés de _lettres" << std::endl;
+    return 0;
+  }
+  return 1;
+}
+
+
+int Enigme3::codage_mot_planete(std::string mot) const
+{
+
+  int code=0;
+  int z=pow(10,mot.length()-1);
+
+  for(std::string::size_type i=0; i<mot.length(); i++)
+  {
+      if(!lettre_existe(mot[i])
+      {
+          return -1;
+      }
+      code+=z*_lettres[mot[i]];
+      z=z/10;
+  }
+  return code;
+}
+
+
+void codage_mots_planete() const
+{
+  _code_solution=0;
+
+  // Lecture de la _chaine avec des délimiteurs
+  std::string const delims{ "-+" }; // délimiteurs
+
+  size_t beg, pos = 0;
+  while ((beg = _chaine.find_first_not_of(delims, pos)) != std::string::npos)
+  {
+    pos = _chaine.find_first_of(delims, beg + 1);
+
+    if(beg==0)
+    {
+        _code_solution+=codage_mot_planete(_chaine.substr(beg, pos - beg),_lettres);
+    }
+
+    if(_chaine[pos]== '+')
+    {
+        _code_solution+=codage_mot_planete(_chaine.substr(beg, pos - beg),_lettres);
+    }
+    else if(_chaine[pos]== '-')
+    {
+        _code_solution-=codage_mot_planete(_chaine.substr(beg, pos - beg),_lettres);
+    }
+  }
+}
--- enigme3.hh
+++ enigme3.hh
@@ -1,16 +1,34 @@
 #pragma once
 
-#include <iostream>
+# include <iostream>
+# include <map>
+# include <math.h>
 #include "enigme.hh"
 
 
 class Enigme3 : public Enigme{ // énigme avec le tableau et le coffre-fort
 
 private:
+  std::map<char,int> _lettres; // ce type d'attribut est commun à Enigme2
+  std::string _chaine;         // ce type d'attribut est commun à Enigme2
 
 
 public:
 
+  // Constructeur
+  Enigme3(std::string nom_carte, int id, int id_enigme, int nb_essais, std::string chaine);
 
+  // Destructeur : bail virtuel ?? ou alors hérite direct de celui de la classe mère ??
+
+  // Getter
+  //std::map<char,int> get_lettres() const {return _lettres};
+  std::string get_chaine() const {return _chaine};
+
+
+  // Méthodes
+  bool lettre_existe(char l) const; // OU bool lettre_existe(const char l) const; ??
+
+  int codage_mot_planete(std::string mot) const;
+  void codage_mots_planete() const; // chaine = mots
 
 };
