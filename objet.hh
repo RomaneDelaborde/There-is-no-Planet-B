ec4932d8c1e1910f96e6dd42b963653fba4bcdf3
Romane Delaborde <romane.delaborde@gmail.com>
2022-01-02 22:34:13 +0100

    _objets_combinables devient _id_objets_combinables

--- objet.hh
+++ objet.hh
@@ -10,19 +10,19 @@ class Objet : public Carte{
 
 private:
   // bool _union; // vaut 1 s'il faut combiner juste l'un des objets combinable de la liste et 0 s'il faut les combiner au moins une fois avec tous les objets de la liste -> on devrait pas en avoir besoin
-  std::map<int, int> _objets_combinables; // map : en clé les id des objets combinables avec cet objet et en valeur : id de la carte résultante de cette combinaison
+  std::map<int, int> _id_objets_combinables; // map : en clé les id des objets combinables avec cet objet et en valeur : id de la carte résultante de cette combinaison
 
 
 public:
 
   // Constructeur
-  Objet(std::string nom_carte, int id, std::map<int, int> objets_combinables): Carte(nom_carte, id), _objets_combinables(objets_combinables){}; // héritage constructeur classe mère
+  Objet(std::string nom_carte, int id, std::map<int, int> objets_combinables): Carte(nom_carte, id), _id_objets_combinables(objets_combinables){}; // héritage constructeur classe mère
 
   // Destructeur : bail virtuel ?? ou alors hérite direct de celui de la classe mère ??
 
   // Méthodes
 
-  // Getter _objets_combinables
-  std::map<int, int> get_objets_combinables() const {return _objets_combinables;};
+  // Getter _id_objets_combinables
+  std::map<int, int> get_id_objets_combinables() const {return _id_objets_combinables;};
 
 };
