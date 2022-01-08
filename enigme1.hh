390b23f848a07f39586ef59aeba1e37f3080a8f7
Romane Delaborde <romane.delaborde@gmail.com>
2022-01-08 10:50:54 +0100

    Classe Enigme1 finie normalement mais pas de tests effectués

--- enigme1.hh
+++ enigme1.hh
@@ -7,10 +7,19 @@
 class Enigme1 : public Enigme{ // énigme avec la radio et les fréquences
 
 private:
+  int _alpha;
+  int _beta;
+  int _gamma;
+  int _val1;
+  int _val2;
+  int _val3;
 
 
 public:
+  // Constructeurs
+  Enigme1(std::string nom_carte, int id, std::vector<int> id_cartes_kick, int nb_essais, int code_solution, int id_carte_debloquee):Enigme(nom_carte, id, id_cartes_kick, nb_essais, code_solution, id_carte_debloquee){};
+  Enigme1(std::string nom_carte, int id, std::vector<int> id_cartes_kick, int nb_essais, int id_carte_debloquee, int alpha, int beta, int gamma);
 
-
+  // Méthodes
 
 };
